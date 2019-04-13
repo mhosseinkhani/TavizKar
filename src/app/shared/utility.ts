08396ec0cde79b2ec499a82049f310d18c54e2b6
Mostafa <mhosseinkhani@yahoo.com>
2019-04-13 18:57:36 +0430

    ser

--- src/app/shared/utility.ts
+++ src/app/shared/utility.ts
@@ -1,6 +1,6 @@
 export class StringHelper {
-  convertNumbers2English(str) {
-    str.replace(
+  public ConvertNumbers2English(str) {
+  return  str.replace(
       /([٠١٢٣٤٥٦٧٨٩])|([۰۱۲۳۴۵۶۷۸۹])/g,
       (m, $1) => m.charCodeAt(0) - ($1 ? 1632 : 1776)
     );
