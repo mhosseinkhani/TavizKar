4f1ec87c27e82ebb26159c1ffd16a3a61ff9a263
Mostafa <mhosseinkhani@yahoo.com>
2019-04-14 17:58:55 +0430

    ....

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,18 +1,42 @@
-import {Component, OnInit} from '@angular/core';
+import { UserService } from "src/app/shared/User.Service";
+import { Component, OnInit } from "@angular/core";
+import { ToastrManager } from "ng6-toastr-notifications";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  selector: "app-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.css"]
 })
 export class MenuComponent implements OnInit {
-
   actualVersion: string;
-clientQty=1250;
-  constructor() {
+  clientQty = 1250;
+  isToken = false;
+  constructor(private service: UserService, public toastr: ToastrManager) {
+    if (window.localStorage.getItem("token")) {
+      this.isToken = true;
+    }
   }
 
   ngOnInit() {
-    this.actualVersion ='اتومکانیک اکبر'; //environment.version;
+    this.actualVersion = "اتومکانیک اکبر"; //environment.version;
+  }
+
+  callMe() {
+    const tick = {
+      Subject: "درخواست تماس",
+      Passage: window.localStorage.getItem("userInfo")
+    };
+    this.service.addTicket(tick).subscribe(
+      res => {
+        this.toastr.infoToastr(
+          "همکاران ما در سریع ترین زمان ممکن ا شما تماس خواهند گرفت",
+          "ارسال",
+          {
+            toastTimeout: 1500
+          }
+        );
+      },
+      error => {}
+    );
   }
 }
